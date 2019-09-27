c9131c1cf3d045c7fa20958831371a5259bfee84
Benjamin Andrew <bandrew01@qub.ac.uk>
2019-09-27 22:21:20 +0100

    increase spare URL tolerance

--- image_fetcher/download_page.py
+++ image_fetcher/download_page.py
@@ -5,9 +5,9 @@ from math import ceil
 
 wait_time = 1
 #As some URL's will likely fail, It's a good idea to fetch more than we need
-#The backup_threshold is what percentage extra to fetch (i.e. if 200 images were
-# needed it would fetch 220 URL's)
-backup_threshold = 0.1
+#The backup_threshold is what amount to multiple needed URL's by
+#At a rating of 2, for 200 images we'd fetch 400 URL's (leaving 200 to spare)
+backup_threshold = 2
 google_images = 'https://www.google.com/search?tbm=isch&q='
 
 options = webdriver.ChromeOptions()
@@ -21,7 +21,7 @@ def round_up_to_nearest_hundred(x):
 def download_page(search_term, total_images):
     browser = webdriver.Chrome('chromedriver.exe', options=options)
     browser.get(google_images+search_term)
-    total_images *= 1 + backup_threshold
+    total_images *= backup_threshold
     required_scrolls = int(round_up_to_nearest_hundred(total_images) / 100)-1
     for i in range(0, required_scrolls):
         browser.execute_script('window.scrollTo(0,document.body.scrollHeight);')

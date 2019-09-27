53ffd9606679f6d8bec5dbf3e2587a63e46b5831
Benjamin Andrew <bandrew01@qub.ac.uk>
2019-09-27 21:46:12 +0100

    Add docstrings

--- image_fetcher/get_image_urls.py
+++ image_fetcher/get_image_urls.py
@@ -4,17 +4,38 @@ from json import loads
 
 
 def download_url(url, headers):
+    """
+    Read data from a given URL
+
+    Parameters:
+    url (str): URL to retrieve data from
+    headers (dict): Headers for urllib to use when requesting from urls (must include 'User-Agent')
+
+    Returns:
+    bytes: Returns bytes of data from the URL
+    """
     req = request.Request(url, headers=headers)
     resp = request.urlopen(req)
     data = resp.read()
     resp.close()
     return data
 
-def get_image_urls(page, text_file=True, verbose=True):
+def get_image_urls(page, verbose=True):
+    """
+    Get a list of URL's from a given google search page
+
+    Parameters:
+    page (str): HTML source of a google image search
+    verbose (bool): Whether to print total URL's found (defaults to True)
+
+    Returns:
+    list: Returns list of image URL's
+    """
     image_urls = []
+    #Get all image source URLS
     for image_html in page.find_all("div",{"class":"rg_meta"}):
         try:
-            #Return the image source and where it searched up to
+            #Add the image source to urls
             final_image_url = loads(image_html.text)["ou"]
             image_urls.append(final_image_url)
         except:

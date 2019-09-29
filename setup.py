4fc0584d144c3bd1be60cd57528ec719ba9b4a2a
Benjamin Andrew <bandrew01@qub.ac.uk>
2019-09-29 13:06:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="image_fetcher",
-    version="1.0.1",
+    version="1.0.2",
     author="Ben Andrew",
     author_email="benandrew89@gmail.com",
     description="Package for downloading images from google",
@@ -17,6 +17,19 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "beautifulsoup4>=4.8.0",
+        "bs4>=0.0.1",
+        "certifi>=2019.9.11",
+        "chardet>=3.0.4",
+        "func-timeout>=4.3.5",
+        "idna>=2.8",
+        "selenium>=3.141.0",
+        "soupsieve>=1.9.3",
+        "tqdm>=4.36.1",
+        "urllib3>=1.25.5",
+        "wincertstore>=0.2",
     ],
     python_requires='>=3.6',
 )

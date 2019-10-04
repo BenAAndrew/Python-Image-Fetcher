74c0de03bace1c133cef1bac171795cd358bd7c7
Benjamin Andrew <bandrew01@qub.ac.uk>
2019-10-04 22:30:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="image_fetcher",
-    version="1.1.2",
+    version="1.1.3",
     author="Ben Andrew",
     author_email="benandrew89@gmail.com",
     description="Package for downloading images from google",

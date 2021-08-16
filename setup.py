fe8307893659ce49fcc2ff0845e11c93b23a2350
BenAAndrew <bandrew01@qub.ac.uk>
2021-08-16 19:50:12 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r") as f:
-    install_requires = f.read().splitlines()
-
-setuptools.setup(
+setup(
     name="image_fetcher",
     version="2.0.0",
     author="Ben Andrew",
@@ -15,12 +12,16 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BenAAndrew/Python-Image-Fetcher",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=install_requires,
+    install_requires=[
+        "func-timeout==4.3.5",
+        "tqdm==4.62.0",
+        "urllib3==1.26.6",
+    ],
     python_requires=">=3.5",
 )
